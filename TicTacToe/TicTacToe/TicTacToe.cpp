64718ae1d95a5ae63e96d4409af9b4a13caf84a8
HHKimryanov19 <HHKimryanov19@codingburgas.bg>
2020-12-02 00:05:48 +0200

    Add option to play vs computer

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,6 +1,137 @@
-#include <iostream>
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
 using namespace std;
  char position[9] = { '1','2','3','4','5','6','7','8','9' };
+ int player = 1;
+
+ void choice(int playerChoice,char sign)
+ {
+     switch (playerChoice)
+     {
+     case 1:
+         if (position[0] == '1')
+         {
+             position[0] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 2:
+         if (position[1] == '2')
+         {
+             position[1] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 3:
+         if (position[2] == '3')
+         {
+             position[2] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 4:
+         if (position[3] == '4')
+         {
+             position[3] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 5:
+         if (position[4] == '5')
+         {
+             position[4] = sign;
+         }
+         else
+         {
+
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 6:
+         if (position[5] == '6')
+         {
+             position[5] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 7:
+         if (position[6] == '7')
+         {
+             position[6] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 8:
+         if (position[7] == '8')
+         {
+             position[7] = sign;
+         }
+         else
+         {
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     case 9:
+         if (position[8] == '9')
+         {
+             position[8] = sign;
+         }
+         else
+         {
+
+             cout << "Invalid move. Place player " << player << " press Enter and try again.";
+             player--;
+             cin.ignore();
+             cin.get();
+         }
+         break;
+     }
+ }
+
 int checkWin()
 {
     if (position[0] == position[1] && position[1] == position[2])
@@ -72,11 +203,97 @@ int checkWin()
 
 int main()
 {
-   
-    int player = 1, choice, n = 0;
-
-    do
+    cout << "/*___*\\" << endl;
+    cout << "\\_____/" << endl;
+    cout << "Hello, I'm Steve." << endl;
+    cout << "Do you want to play together my favorite game tic tac toe? If you want type Y, otherwise press Enter and play with your friend." << endl;
+    cout << "You should know that I am not very good and sometimes I want to put my symbol in a place where it already exists.Then press Enter." << endl;
+    char playWithSteve;
+    cin.get(playWithSteve);
+    int playerChoice, n = 0;
+    system("CLS");
+    if(playWithSteve!='Y')
     {
+        do
+        {
+            cout << "Tic Tac Toe" << endl;
+
+            cout << "Player 1 (X)  -  Player 2 (O)" << endl;
+            cout << endl;
+            cout << " _____" << " _____ " << "_____" << endl;
+            cout << "|     |     |     |" << endl;
+            cout << "|  " << position[0] << "  |  " << position[1] << "  |  " << position[2] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl;
+            cout << "|     |     |     |" << endl;
+
+            cout << "|  " << position[3] << "  |  " << position[4] << "  |  " << position[5] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl;
+            cout << "|     |     |     |" << endl;
+
+            cout << "|  " << position[6] << "  |  " << position[7] << "  |  " << position[8] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl << endl;
+            char sign;
+
+            player = (player % 2) ? 1 : 2;
+            cout << "Player " << player << ", enter a number:  ";
+            cin >> playerChoice;
+            sign = (player == 1) ? 'X' : 'O';
+            choice(playerChoice,sign);
+            player++;
+            n = checkWin();
+            cout << endl;
+            system("CLS");
+        } while (n == -1);
+    }
+    else
+    {
+        char sign;
+        do
+        {
+            cout << "Tic Tac Toe" << endl;
+
+            cout << "Player 1 (X)  -  Player 2 (O)" << endl;
+            cout << endl;
+            cout << " _____" << " _____ " << "_____" << endl;
+            cout << "|     |     |     |" << endl;
+            cout << "|  " << position[0] << "  |  " << position[1] << "  |  " << position[2] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl;
+            cout << "|     |     |     |" << endl;
+
+            cout << "|  " << position[3] << "  |  " << position[4] << "  |  " << position[5] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl;
+            cout << "|     |     |     |" << endl;
+
+            cout << "|  " << position[6] << "  |  " << position[7] << "  |  " << position[8] << "  |" << endl;
+
+            cout << "|_____|_____|_____|" << endl << endl;
+            player = (player % 2) ? 1 : 2;
+            if (player == 1)
+            {
+                cout << "Player " << player << ", enter a number:  ";
+                cin >> playerChoice;
+                sign = (player == 1) ? 'X' : 'O';
+                choice(playerChoice, sign);
+            }
+            else
+            {
+                cout << "Player " << player << ", enter a number:  ";
+                srand(time(NULL));
+                playerChoice = rand() % 9 + 1;
+                cout << playerChoice << endl;
+                sign = (player == 1) ? 'X' : 'O';
+                choice(playerChoice, sign);
+            }
+            n = checkWin();
+            player++;
+            system("CLS");
+        } while (n == -1);
+    }
         cout << "Tic Tac Toe" << endl;
 
         cout << "Player 1 (X)  -  Player 2 (O)" << endl;
@@ -96,179 +313,19 @@ int main()
         cout << "|  " << position[6] << "  |  " << position[7] << "  |  " << position[8] << "  |" << endl;
 
         cout << "|_____|_____|_____|" << endl << endl;
-        char sign;
-
-        player = (player % 2) ? 1 : 2;
-        cout << "Player " << player << ", enter a number:  ";
-        cin >> choice;
-        sign = (player == 1) ? 'X' : 'O';
-        switch (choice)
+        if (n == 1)
         {
-        case 1:
-            if (position[0] == '1')
-            {
-                position[0] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 2:
-            if (position[1] == '2')
-            {
-                position[1] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 3:
-            if (position[2] == '3')
-            {
-                position[2] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 4:
-            if (position[3] == '4')
-            {
-                position[3] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 5:
-            if (position[4] == '5')
-            {
-                position[4] = sign;
-            }
-            else
-            {
-
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 6:
-            if (position[5] == '6')
-            {
-                position[5] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 7:
-            if (position[6] == '7')
-            {
-                position[6] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 8:
-            if (position[7] == '8')
-            {
-                position[7] = sign;
-            }
-            else
-            {
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
-        case 9:
-            if (position[8] == '9')
-            {
-                position[8] = sign;
-            }
-            else
-            {
-
-                cout << "Invalid move. Place player " << player << " press Enter and try again.";
-                player--;
-                cin.ignore();
-                cin.get();
-                n--;
-            }
-            break;
+            cout << "Congratulation, player " << --player << "!" << endl;
+            cout << "/*___*\\" << endl;
+            cout << "\\_____/" << endl;
+            cout << "See you again.Bye" << endl;
         }
-        player++;
-        n=checkWin();
-        cout << endl;
-        system("CLS");
-    } while (n ==-1);
-    cout << "Tic Tac Toe" << endl;
+        else
+        {
+            cout << "==> Game draw <==";
+        }
+        cin.ignore();
+        cin.get();
+        return 0;
 
-    cout << "Player 1 (X)  -  Player 2 (O)" << endl;
-    cout << endl;
-    cout << " _____" << " _____ " << "_____" << endl;
-    cout << "|     |     |     |" << endl;
-    cout << "|  " << position[0] << "  |  " << position[1] << "  |  " << position[2] << "  |" << endl;
-
-    cout << "|_____|_____|_____|" << endl;
-    cout << "|     |     |     |" << endl;
-
-    cout << "|  " << position[3] << "  |  " << position[4] << "  |  " << position[5] << "  |" << endl;
-
-    cout << "|_____|_____|_____|" << endl;
-    cout << "|     |     |     |" << endl;
-
-    cout << "|  " << position[6] << "  |  " << position[7] << "  |  " << position[8] << "  |" << endl;
-
-    cout << "|_____|_____|_____|" << endl << endl;
-    if (n == 1)
-    {
-        cout << "Congratulation "<<--player << "!" << endl;
-        cout << "/*___*\\" << endl;
-        cout << "\\_____/" << endl;
-        
-    }
-    else
-    {
-        cout << "==>\aGame draw";
-    }
-    cin.ignore();
-    cin.get();
-    return 0;
 }
